800aeed19d2ce3e318f15641c21b336e6324339f
Renato Martins <rmartins6694@gmail.com>
2022-10-19 09:01:53 -0300

    changes in main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-const robotron = document.querySelector(".robo");
+//const robotron = document.querySelector(".robo");
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
-robotron.addEventListener("click", () => {
-    dizOi();
+somar.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) + 1;
 });
 
-function dizOi () {
-    console.log('oi');
-}
+subtrair.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) + 1;
+});
